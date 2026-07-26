c6a3f40b7dcca5d7eb67c84d3be615cca27ddebc
littlefly365 <c081526@hotmail.com>
2026-07-26 15:36:27 -0500

    remove sysctl implementation

--- sysctl/kern.h
+++
@@ -1,6 +0,0 @@
-#ifndef _KERN_H
-#define _KERN_H	1
-
-int getostype(char *buf, size_t buflen);
-
-#endif
--- sysctl/proc.h
+++
@@ -1,6 +0,0 @@
-#ifndef _PROC_H
-#define _PROC_H	1
-
-int read_proc(const char *path, char *buf, size_t buflen);
-
-#endif
