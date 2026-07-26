10fd4b3a9ce89fd8d27e788a716955d823f3fe5f
littlefly365 <c081526@hotmail.com>
2026-07-26 10:07:19 -0500

    cmdinfo: improve changes and descriptions

--- cmdinfo/cmdinfo.h
+++ cmdinfo/cmdinfo.h
@@ -27,11 +27,11 @@ static const table_t cmd_tbl[] = {
 	{ "echo", "1.0", "Display all passed arguments to standard output", "NetBSD", "Stable", "None" },
 	{ "env", "1.0", "Print or modify the environment variables", "NetBSD", "Stable", "None" },
 	{ "hostname", "1.0", "Print or set the system hostname", "NetBSD", "Stable", "None" },
-	{ "ldyd", "1.0", "List the dynamic dependencies of a executable", "AtlanticOS", "Stable", "None" },
+	{ "ldyd", "1.0", "List the dynamic dependencies of an executable", "AtlanticOS", "Stable", "None" },
 	{ "printenv", "1.0", "Print the environment variables", "NetBSD", "Stable", "None"},
 	{ "pwd", "1.0", "Print the current working directory", "NetBSD", "Stable", "None" },
 	{ "sw_vers", "1.1", "A reimplementation of the macOS command", "AtlanticOS", "Stable", "Update command for compatibility"
-										               " with modern macOS version" },
+										               " with modern macOS" },
 	{ "sync", "1.0", "Call to the sync syscall", "NetBSD", "Stable", "None" },
 	{ "uname", "1.2", "Print system information", "NetBSD", "Stable",  "\n- Remove sysctl dependency"
 									   "\n- Add -o option to print os name"
