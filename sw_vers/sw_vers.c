bb8387eaa148e1fa6abf92c2bdfa01eb7e20e9e9
littlefly365 <c081526@hotmail.com>
2026-07-25 20:43:36 -0500

    sw_vers: update sw_vers for compatibility with modern macOS

--- sw_vers/sw_vers.c
+++ sw_vers/sw_vers.c
@@ -3,8 +3,9 @@
 #include <string.h>
 
 static const char ProductName[] = "AtlanticOS";
-static const char ProductVersion[] = "26B";	/* 26 => 2026 */ /* AtlanticOS has 2 cycles, A (Jan-Jun) and B (Jul-Dec) */
-#if defined(BUILD_VERSION)			/* This must be defined on official release builds */
+static const char ProductVersion[] = "26.0";		/* 26 => 2026 */ 
+static const char ProductVersionExtra[] = "(b)";	/* AtlanticOS has 2 cycles, A (Jan-Jun) and B (Jul-Dec) */
+#if defined(BUILD_VERSION)				/* This must be defined on official release builds */
 static const char BuildVersion[] = BUILD_VERSION;
 #else
 static const char BuildVersion[] = __DATE__;
@@ -14,32 +15,45 @@ static void
 usage(void)
 {
 	printf("usage: %s\n"
-		"  or:  %s -productName\n"
-		"  or:  %s -productVersion\n"
-		"  or:  %s -buildVersion\n",
-		getprogname(), getprogname(), getprogname(), getprogname());
+		"  or:  %s --productName\n"
+		"  or:  %s --productVersion\n"
+		"  or:  %s --productVersionExtra\n"
+		"  or:  %s --buildVersion\n",
+		getprogname(), getprogname(), getprogname(), getprogname(), getprogname());
 	exit(0);
 }
 
 int
 main(int argc, char *argv[])
 {
-	if (argc > 1) {
-		if (!strcmp(argv[1], "-productName"))
+	if (argc > 2) {
+		printf("%s: extra operand '%s'\n", getprogname(), argv[2]);
+		printf("Try '%s --help' for more information.\n", getprogname());
+		return 1;
+	}
+
+	if (argc == 2) {
+		if (!strcmp(argv[1], "--productName") || !strcmp(argv[1], "-productName"))
 			printf("%s\n", ProductName);
-		else if (!strcmp(argv[1], "-productVersion"))
+		else if (!strcmp(argv[1], "--productVersion") || !strcmp(argv[1], "-productVersion"))
 			printf("%s\n", ProductVersion);
-		else if (!strcmp(argv[1], "-buildVersion"))
+		else if (!strcmp(argv[1], "--productVersionExtra") || !strcmp(argv[1], "-productVersionExtra"))
+			printf("%s\n", ProductVersionExtra);
+		else if (!strcmp(argv[1], "--buildVersion") || !strcmp(argv[1], "-buildVersion"))
 			printf("%s\n", BuildVersion);
-		else if (!strcmp(argv[1], "--help") || !strcmp(argv[1], "-h"))
+		else if (!strcmp(argv[1], "--help") || !strcmp(argv[1], "-help") || !strcmp(argv[1], "-h"))
 			usage();
 		else {
 			fprintf(stderr, "%s: invalid option '%s'\n", getprogname(), argv[1]);
+			fprintf(stderr, "Try '%s --help' for more information.\n", getprogname());
 			return 1;
 		}
-	} else {
-		printf("ProductName: %s\nProductVersion: %s\nBuildVersion: %s\n",
-			ProductName, ProductVersion, BuildVersion);
+	} else if (argc == 1) {
+		printf( "ProductName:           %s\n"
+			"ProductVersion:        %s\n"
+			"ProductVersionExtra:   %s\n"
+			"BuildVersion:          %s\n",
+			ProductName, ProductVersion, ProductVersionExtra, BuildVersion);
 	}
 
 	return 0;
